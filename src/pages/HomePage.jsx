d5cc26b1dd9cf3777a4f981055bc62f7ded80d27
DaviDChoudharY <122169398+DaviDChoudharY@users.noreply.github.com>
2025-06-23 12:25:18 +0530

    fix: Adjust import path for Hotelssection in HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import LandingPage from "./LandingPage"; // Adjust path as needed
 import HotelCitiesSection from "./HotelCitiesSection";
-import HotelsSection from "./Hotelssection";
+import HotelsSection from "./HotelsSection";
 import MorePage from "./MorePage";
 import InfoSection from "./InfoPage";
 
